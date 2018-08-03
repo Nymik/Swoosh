9b3a92130100013aff61af2b1e68116961063a3f
husarik <jakub.husarik@gmail.com>
2018-08-03 10:13:25 +0200

    lesson-02

---
+++ app/src/main/java/com/udemy/swoosh/LeagueActivity.kt
@@ -0,0 +1,13 @@
+package com.udemy.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+
+    }
+}
