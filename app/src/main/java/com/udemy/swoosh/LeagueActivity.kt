1fbefa88d7f84bfaf5fcfeccbf4d98a62e98e921
husarik <jakub.husarik@gmail.com>
2018-08-03 13:53:49 +0200

    lesson-03

--- app/src/main/java/com/udemy/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/udemy/swoosh/LeagueActivity.kt
@@ -1,7 +1,9 @@
 package com.udemy.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
@@ -9,5 +11,9 @@ class LeagueActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
+        BtnGetStarted.setOnClickListener {
+            val welcomeIntent = Intent(this, WelcomeActivity::class.java)
+            startActivity(welcomeIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/udemy/swoosh/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.udemy.swoosh
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BtnGetStarted.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+
+
+        }
+    }
+}
